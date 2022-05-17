9c4ffc3ad78155ce36c4d99d2271de8ef399db43
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-17 16:19:58 +0300

    Hit the in-house `starwars` API and get characters in order
    API = https://swapi-api.hbtn.io
    Use callbacks to handle the async nature of request module
    
    For task 0 of Solo-Project: - `0x06-starwars_api`

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+function getCharsList (_, __, body) {
+  const info = JSON.parse(body).characters;
+  handleCharsList(info);
+}
+
+request(url, getCharsList);
+
+function handleCharsList (value) {
+  const arr = [];
+
+  const printCharsList = (value2, idx, charCount) => {
+    arr.push([value2, idx]);
+    if (arr.length === charCount) {
+      const newArr = arr.sort((a, b) => a[1] - b[1]);
+      for (let i = 0; i < newArr.length; i++) {
+        console.log(newArr[i][0]);
+      }
+    }
+  };
+  for (let i = 0; i < value.length; i++) {
+    const handleCharsName = (_, __, body) => {
+      const info = JSON.parse(body).name;
+      printCharsList(info, i, value.length);
+    };
+    request(value[i], handleCharsName);
+  }
+}
