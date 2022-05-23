1c6d11d61f27f4b7a539a64df014b2798e07deb2
Leykun Gizaw <bisrat_aregawi1914@yahoo.com>
2022-05-23 07:09:56 +0300

    Make a square matrix rotator function
    For task 0 of Solo-Project: - `0x07-rotate_2d_matrix`

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Module defines `rotate_2d_matrix` function"""
+
+
+from typing import Any, List
+
+
+def rotate_2d_matrix(matrix: List[List[Any]]) -> None:
+    """In-place rotate a 2d square matrix 90 deg CW
+
+    Args:
+        matrix: list representation of a 2d square matrix
+
+    Returns:
+        None
+    """
+    order = len(matrix)
+    for i in range(order):
+        for j in range(i, order):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = tmp
+
+    for row in matrix:
+        row.reverse()
